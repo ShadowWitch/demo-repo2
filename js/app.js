83a6a49682b68faa7b3a64f4ea1be7e3f0bbfc51
Lucas <lucas@gmail.com>
2020-10-20 14:12:21 -0600

    Se agregaron cosas

--- js/app.js
+++ js/app.js
@@ -3,7 +3,23 @@
 console.log('Hola Mundo')
 
 
+/*
 
+
+git config --global user.name 'Nombre'
+git config --blobal user.email 'Email'
+git config --global -l
+
+git init
+git status
+git add .
+git commit -m "Descripcion de lo que se hizo"
+git diff
+git checkout .
+git log
+
+
+*/
 
 
 
---
+++ js/validaciones.js
@@ -0,0 +1,8 @@
+
+
+console.log('Validacionesss!!!!')
+
+
+
+
+// qwe
